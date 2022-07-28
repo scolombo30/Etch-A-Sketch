7771d2b9fc834ccf6c7ff072497c9d621de76807
Sebastian Colombo <scolombo30@studenti.uninsubria.it>
2022-07-28 10:36:08 +0200

    Add function to change size of the grid

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const containerGrid = document.querySelector('.containerGrid');
 const containerSize = 600;
-let gridSize=16;
+let gridSize;
+if (localStorage.getItem('gridSize') !== null) {
+    gridSize = localStorage.getItem('gridSize');
+}
+else {gridSize=16;}
 let divWidht = containerSize/gridSize;
 let color = '#000';
 let newDiv;
@@ -58,12 +62,19 @@ function reset(){
 function changeSizeSmall(){
     console.log("click clear 16");
     gridSize = 16;
+    saveGridSizeAndReload();
 }
 function changeSizeMedium(){
     console.log("click clear 32");
     gridSize = 32;    
+    saveGridSizeAndReload();
 }
 function changeSizeLarge(){
     console.log("click clear 64");
     gridSize = 64;
+    saveGridSizeAndReload();
+}
+function saveGridSizeAndReload(){
+    localStorage.setItem('gridSize', gridSize);
+    window.location.reload();
 }
